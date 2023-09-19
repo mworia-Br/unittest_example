9a2a435145cc3ea89231c4af59e8417dca59671f
Brian Nganga <briannganga70@gmail.com>
2023-09-19 13:06:32 +0300

    initial

---
+++ calc.py
@@ -0,0 +1,20 @@
+# We will use this to perform tests
+
+def add(x, y):
+    """Add function"""
+    return x + y
+
+def subtract(x, y):
+    """Subtract function"""
+    return x - y
+
+def multiply(x, y):
+    """Multiply function"""
+    return x * y
+
+def divide(x, y):
+    """Divide function"""
+    if y == 0:
+        raise ValueError('Can not divide by zero')
+    return x / y
+
